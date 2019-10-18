9aa840fdfc094b620d533bd5864af8b9dd33f629
David Lodi <lodi.dev@outlook.com>
2019-10-17 23:55:30 -0500

    menu en un ROW

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -6,8 +6,7 @@ class ContadorPage extends StatefulWidget {
 }
 
 class _ContadorPageState extends State<ContadorPage> {
- 
- final _estiloTexto = new TextStyle(fontSize: 25);
+  final _estiloTexto = new TextStyle(fontSize: 25);
 
   int _conteo = 0;
 
@@ -15,36 +14,54 @@ class _ContadorPageState extends State<ContadorPage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text('STATE'),
-        centerTitle: true,
-        elevation: 0,
+        appBar: AppBar(
+          title: Text('STATE'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Número de taps:',
+                style: _estiloTexto,
+              ),
+              Text(
+                _conteo.toString(),
+                style: _estiloTexto,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: _crearBotones());
+  }
+
+  Widget _crearBotones() {
+    return Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+       SizedBox(
+        width: 30,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Número de taps:',
-              style: _estiloTexto,
-            ),
-            Text(
-              _conteo.toString(),
-              style: _estiloTexto,
-            )
-          ],
+      FloatingActionButton(
+        child: Icon(Icons.exposure_zero),
+        onPressed: null,
+      ),
+      Expanded(
+        child: SizedBox(
+         
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 0,
-        child: Icon(Icons.add),
-        onPressed: () {
-        setState(() {
-          _conteo++;
-        });
-        },
-        
+      FloatingActionButton(
+        child: Icon(Icons.remove),
+        onPressed: null,
       ),
-    );
+      SizedBox(
+        width: 5.0,
+      ),
+      FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    ]);
   }
 }
