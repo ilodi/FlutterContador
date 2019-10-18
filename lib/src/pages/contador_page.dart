0837fd0aa0df413c229e14b0f192fefed68cd0f3
David Lodi <lodi.dev@outlook.com>
2019-10-18 00:08:51 -0500

    listo la primer practica de flutter

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -39,29 +39,39 @@ class _ContadorPageState extends State<ContadorPage> {
 
   Widget _crearBotones() {
     return Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-       SizedBox(
+      SizedBox(
         width: 30,
       ),
       FloatingActionButton(
         child: Icon(Icons.exposure_zero),
-        onPressed: null,
+        onPressed: _reset,
       ),
       Expanded(
-        child: SizedBox(
-         
-        ),
+        child: SizedBox(),
       ),
       FloatingActionButton(
         child: Icon(Icons.remove),
-        onPressed: null,
+        onPressed: _sustraer,
       ),
       SizedBox(
         width: 5.0,
       ),
       FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: null,
+        onPressed: _agregar,
       ),
     ]);
+  }
+
+  void _agregar() {
+    setState(() => _conteo++);
+  }
+
+  void _sustraer() {
+    setState(() => _conteo--);
+  }
+
+  void _reset() {
+    setState(() => _conteo = 0);
   }
 }
